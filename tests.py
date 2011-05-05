e02d865d50facff750bf9b2d51979f041ebe8abc
James Cleveland <jamescleveland@gmail.com>
2011-05-05 06:53:43 +0100

    Added tests from whirlpool.py

---
+++ tests.py
@@ -0,0 +1,9 @@
+from whirlpool import hash
+from binascii import b2a_hex
+
+assert b2a_hex(hash('The quick brown fox jumps over the lazy dog')) == \
+    'b97de512e91e3828b40d2b0fdce9ceb3c4a71f9bea8d88e75c4fa854df36725fd2b52eb6544edcacd6f8beddfea403cb55ae31f03ad62a5ef54e42ee82c3fb35'
+assert b2a_hex(hash('The quick brown fox jumps over the lazy eog')) == \
+    'c27ba124205f72e6847f3e19834f925cc666d0974167af915bb462420ed40cc50900d85a1f923219d832357750492d5c143011a76988344c2635e69d06f2d38c'
+assert b2a_hex(hash('')) == \
+    '19fa61d75522a4669b44e39c1d2e1726c530232130d407f89afee0964997f7a73e83be698b288febcf88e3e03c4f0757ea8964e59b63d93708b138cc42a66eb3'
