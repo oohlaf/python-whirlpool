c80359e6754795d5812e4dc5569d06eb0c3f0e7f
Olaf Conradi <olaf@conradi.org>
2018-02-19 00:47:45 +0100

    Flake8 on test suite

--- test/test_whirlpool.py
+++ test/test_whirlpool.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 import unittest
-import sys
 
 import whirlpool
 
@@ -9,8 +8,10 @@ from binascii import b2a_hex
 
 data = {
     'empty'     : ''.encode('ascii'),
-    'tqbfjotld' : 'The quick brown fox jumps over the lazy dog'.encode('ascii'),
-    'tqbfjotle' : 'The quick brown fox jumps over the lazy eog'.encode('ascii'),
+    'tqbfjotld' : 'The quick brown fox jumps over the lazy '
+                  'dog'.encode('ascii'),
+    'tqbfjotle' : 'The quick brown fox jumps over the lazy '
+                  'eog'.encode('ascii'),
     'tqbf'      : 'The quick brown fox'.encode('ascii'),
     'jotld'     : ' jumps over the lazy dog'.encode('ascii'),
     'jotle'     : ' jumps over the lazy eog'.encode('ascii'),
@@ -19,12 +20,24 @@ data = {
 }
 
 results = {
-    'empty'     : '19fa61d75522a4669b44e39c1d2e1726c530232130d407f89afee0964997f7a73e83be698b288febcf88e3e03c4f0757ea8964e59b63d93708b138cc42a66eb3',
-    'tqbfjotld' : 'b97de512e91e3828b40d2b0fdce9ceb3c4a71f9bea8d88e75c4fa854df36725fd2b52eb6544edcacd6f8beddfea403cb55ae31f03ad62a5ef54e42ee82c3fb35',
-    'tqbfjotle' : 'c27ba124205f72e6847f3e19834f925cc666d0974167af915bb462420ed40cc50900d85a1f923219d832357750492d5c143011a76988344c2635e69d06f2d38c',
-    'tqbf'      : '317edc3c5172ea5987902aa9c4f1defedf4d5aa59209bdf7574cc6da0039852c24b8da70ecb07997ff83e86d32d2851215d3dcbd6bb9736bdef21c349d483e6d',
-    'unicode'   : '2a083e3f53ddc2e4cd003104b0d020a9e7959188289d39c5b58ac9478cbc7f429b851ddce0ca34a668b5f8465175eb1b184afcf306da3afd5bd6f358c16257de',
-    'binary'    : '2a083e3f53ddc2e4cd003104b0d020a9e7959188289d39c5b58ac9478cbc7f429b851ddce0ca34a668b5f8465175eb1b184afcf306da3afd5bd6f358c16257de',
+    'empty'     : '19fa61d75522a4669b44e39c1d2e1726c530232130d'
+                  '407f89afee0964997f7a73e83be698b288febcf88e3'
+                  'e03c4f0757ea8964e59b63d93708b138cc42a66eb3',
+    'tqbfjotld' : 'b97de512e91e3828b40d2b0fdce9ceb3c4a71f9bea8'
+                  'd88e75c4fa854df36725fd2b52eb6544edcacd6f8be'
+                  'ddfea403cb55ae31f03ad62a5ef54e42ee82c3fb35',
+    'tqbfjotle' : 'c27ba124205f72e6847f3e19834f925cc666d097416'
+                  '7af915bb462420ed40cc50900d85a1f923219d83235'
+                  '7750492d5c143011a76988344c2635e69d06f2d38c',
+    'tqbf'      : '317edc3c5172ea5987902aa9c4f1defedf4d5aa5920'
+                  '9bdf7574cc6da0039852c24b8da70ecb07997ff83e8'
+                  '6d32d2851215d3dcbd6bb9736bdef21c349d483e6d',
+    'unicode'   : '2a083e3f53ddc2e4cd003104b0d020a9e7959188289'
+                  'd39c5b58ac9478cbc7f429b851ddce0ca34a668b5f8'
+                  '465175eb1b184afcf306da3afd5bd6f358c16257de',
+    'binary'    : '2a083e3f53ddc2e4cd003104b0d020a9e7959188289'
+                  'd39c5b58ac9478cbc7f429b851ddce0ca34a668b5f8'
+                  '465175eb1b184afcf306da3afd5bd6f358c16257de',
 }
 
 
@@ -100,7 +113,7 @@ class TestWhirlpool(unittest.TestCase):
         self.assertEqual(wp.digest_size, 64)
         with self.assertRaises((AttributeError, TypeError)):
             wp.digest_size = 32
-    
+
     def test_block_size(self):
         wp = whirlpool.new()
         self.assertEqual(wp.block_size, 64)
